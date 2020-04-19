294d72e361773790f67243c12c4bc11eafd9c8b2
Quadtree <quadtree@gmail.com>
2020-04-18 18:01:52 -0700

    Updated

---
+++ actors/MissionPointer.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public class MissionPointer : Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        var ps = GetTree().Root.FindChildByType<PlayerShip>();
+
+        if (ps != null && ps.NextStation != null)
+        {
+            this.Visible = true;
+
+            var dist = ps.GetGlobalLocation().DistanceTo(ps.NextStation.GetGlobalLocation());
+
+            if (dist > 20)
+            {
+                var posDelta = (ps.NextStation.GetGlobalLocation() - ps.GetGlobalLocation()).Normalized();
+                this.SetGlobalLocation(ps.GetGlobalLocation() + posDelta * 20);
+                this.LookAt(ps.NextStation.GetGlobalLocation(), new Vector3(0, 1, 0));
+            } else {
+                this.SetGlobalLocation(ps.NextStation.GetGlobalLocation() + new Vector3(0, 5, 0));
+                this.LookAt(ps.NextStation.GetGlobalLocation(), new Vector3(0, 0, 1));
+            }
+        } else {
+            this.Visible = false;
+        }
+    }
+}
